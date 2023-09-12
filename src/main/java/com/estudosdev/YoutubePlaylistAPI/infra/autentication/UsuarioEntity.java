88e5437dd738085981c6d09e880f79eaf1f5c7fd
Gui <guilherme.g.braz@outlook.com>
2023-09-11 22:10:30 -0300

    usuario para login

---
+++ src/main/java/com/estudosdev/YoutubePlaylistAPI/infra/autentication/UsuarioEntity.java
@@ -0,0 +1,64 @@
+package com.estudosdev.YoutubePlaylistAPI.infra.autentication;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity(name = "usuario")
+@Table(name = "usuario")
+@EqualsAndHashCode(of = "id")
+public class UsuarioEntity implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false, unique = true)
+    private String senha;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
