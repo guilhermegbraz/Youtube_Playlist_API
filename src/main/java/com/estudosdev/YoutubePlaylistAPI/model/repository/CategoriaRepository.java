0f63fc96a57364019d20b3f15d05c1a3b5d99a7b
Gui <guilherme.g.braz@outlook.com>
2023-10-31 19:36:55 -0300

    refactor

--- src/main/java/com/estudosdev/YoutubePlaylistAPI/model/repository/CategoriaRepository.java
+++ src/main/java/com/estudosdev/YoutubePlaylistAPI/model/repository/CategoriaRepository.java
@@ -2,10 +2,12 @@ package com.estudosdev.YoutubePlaylistAPI.model.repository;
 
 import com.estudosdev.YoutubePlaylistAPI.model.entities.CategoriaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<CategoriaEntity, Long> {
 
 
